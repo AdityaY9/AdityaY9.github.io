efcad4e49d63fa08f891dc8181007ad72f483449
Aditya-y9 <aditya.yedurkar@gmail.com>
2024-04-14 17:13:07 +0530

    SEO

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/aditya-y9"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -34,7 +34,7 @@ function Footer() {
             
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://in.linkedin.com/in/aditya-yedurkar-curious"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -44,7 +44,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/soumyajit4419"
+                href="https://www.instagram.com/aditya_.y9"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
